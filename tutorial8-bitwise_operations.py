e0e6f3d88212d2e887dabfb82530b2c421d4c791
Yash singhal <yashsinghalofficialdev@gmail.com>
2025-12-21 22:23:07 +0530

    Add files via upload

---
+++ tutorial8-bitwise_operations.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+img1=np.zeros((250,500,3),np.uint8)
+img1=cv2.rectangle(img1,(200,0),(300,100),(255,255,255),-1)
+img2 = np.zeros((250,500,3), np.uint8)
+img2 = cv2.circle(img2, (250,125), 60, (255,255,255), -1)
+bitAnd=cv2.bitwise_and(img2,img1)
+bitOr=cv2.bitwise_or(img2,img1)
+bitXOR=cv2.bitwise_xor(img2,img1)
+bitNOT=cv2.bitwise_not(img2)
+cv2.imshow("img1",img1)
+cv2.imshow("img2",img2)
+cv2.imshow("bitAND",bitAnd)
+cv2.imshow("bitOR",bitOr)
+cv2.imshow("BITXOR",bitXOR)
+cv2.imshow("bitNot",bitNOT)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
